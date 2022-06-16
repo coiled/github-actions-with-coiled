f444e9b667fb33a76689786d86bfecea2881e138
Richard Pelgrim <68642378+rrpelgrim@users.noreply.github.com>
2022-06-16 07:53:37 +0200

    Update quickstart.py

--- scripts/quickstart.py
+++ scripts/quickstart.py
@@ -24,9 +24,15 @@ ddf = dd.read_parquet(
     storage_options={"anon": True},
 ).persist()
 
+# perform groupby aggregation
 result = ddf.groupby("passenger_count").tip_amount.mean().compute()
-
 print(result)
+
+# write result to S3
+result = dd.from_pandas(result, npartitions=1)
+result.to_parquet(
+    "s3://coiled-datasets/github-actions/quickstart/"
+    )
 
 client.close()
 cluster.close()
